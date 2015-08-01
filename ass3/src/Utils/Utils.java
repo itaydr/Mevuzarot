4e94db6f7d742c59b3fc40d0f6188d271b38d254
Itay Dressler <itaydressler@gmail.com>
2015-08-01 15:11:04 +0300

    Reading fro the multiple input files.

--- ass3/src/Utils/Utils.java
+++ ass3/src/Utils/Utils.java
@@ -1,5 +1,19 @@
 package Utils;
 
-public class Utils {
+import org.apache.hadoop.fs.Path;
 
+public class Utils {
+	
+	private final static int NUMBER_OF_INPUT_PATHS = 10;
+	
+	public static Path[] generateInputPaths() {
+		Path[] paths = new Path[NUMBER_OF_INPUT_PATHS];
+		
+		for (int i = 0 ; i < NUMBER_OF_INPUT_PATHS ; i++) {
+			String p = "s3n://dsp152/syntactic-ngram/biarcs/biarcs." + String.format("%02d", i) +"-of-99";
+			paths[i] = new Path(p);
+		}
+		
+		return paths;
+	}
 }
